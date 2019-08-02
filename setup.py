8558d3cf0e817ceffa9bdfb10c7fdff38198149c
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2019-08-02 12:13:46 -0300

    turns out Python 3.4 doesn't work with TFODAPI

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup(
         'Intended Audience :: Science/Research',
         'Natural Language :: English',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
